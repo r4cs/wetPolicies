52b514c21094beefe9e90b8f2a14d2eba9435e62
r4cs <r.guzansky@hotmail.com>
2024-06-13 21:57:16 -0300

    Update DataSourceConfig.java
    inutilizando DataSourceConfig

--- src/main/java/br/com/GS4/wetPolicies/config/DataSourceConfig.java
+++ src/main/java/br/com/GS4/wetPolicies/config/DataSourceConfig.java
@@ -1,27 +1,27 @@
 package br.com.GS4.wetPolicies.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import javax.sql.DataSource;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import javax.sql.DataSource;
+// import org.springframework.boot.jdbc.DataSourceBuilder;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 
-@Configuration
+// @Configuration
 public class DataSourceConfig {
 
-    private static final Logger logger = LoggerFactory.getLogger(DataSourceConfig.class);
+    // private static final Logger logger = LoggerFactory.getLogger(DataSourceConfig.class);
 
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
+    // @Value("${spring.datasource.url}")
+    // private String dbUrl;
 
-    @Bean
-    public DataSource getDataSource() {
-        logger.info("Configuring DataSource with URL: {}", dbUrl);
-        // For security reasons, avoid logging the password directly
-        return DataSourceBuilder.create()
-                .url(dbUrl)
-                .build();
-    }
+    // @Bean
+    // public DataSource getDataSource() {
+    //     logger.info("Configuring DataSource with URL: {}", dbUrl);
+    //     // For security reasons, avoid logging the password directly
+    //     return DataSourceBuilder.create()
+    //             .url(dbUrl)
+    //             .build();
+    // }
 }
