b897f63aa941005e96a8edc160b70f3b4246c49a
r4cs <r.guzansky@hotmail.com>
2025-09-19 14:03:18 -0300

    feat: Adicionando configurações e arquivos para ambiente local

--- src/main/java/br/com/GS4/wetPolicies/config/SwaggerConfig.java
+++ src/main/java/br/com/GS4/wetPolicies/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI geraDocumentacao() {
         Server devServer = new Server();
-        devServer.setUrl("https://wet-policies.azurewebsites.net/");
+        devServer.setUrl("http://localhost:8080/");
         devServer.description("WetPolicies Api");
 
         Contact contact = getContact();
